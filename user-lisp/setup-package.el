eba3a25916faa630fa16e93e76f78b13ef8843a6
Puneeth Chaganti <punchagan@muse-amuse.in>
2014-08-09 20:27:10 -0400

    Change my/<func-name> to pc/<func-name>.

--- user-lisp/setup-package.el
+++ user-lisp/setup-package.el
@@ -12,12 +12,12 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
-(defun my/write-packages-to-config ()
+(defun pc/write-packages-to-config ()
   "Update my-packages in the config, based on the current-value."
   (delete-dups my-packages)
   (find-function-do-it 'my-packages 'defvar 'switch-to-buffer)
   (kill-sexp)
-  (insert (format "(defvar my-packages \n'%s)" my-packages))
+  (insert (format "(defvar my-packages \n\n'%s)" my-packages))
   (backward-sexp)
   (fill-paragraph)
   (save-buffer)
@@ -28,13 +28,13 @@
   "Update the my-packages variable in the config."
   (add-to-list 'my-packages (if (package-desc-p pkg)
 				(package-desc-name pkg) pkg))
-  (my/write-packages-to-config))
+  (pc/write-packages-to-config))
 
 ;; Advice package-delete to update the package list in the config.
 (defadvice package-delete (after delete-update-package-list (pkg))
   "Update the my-packages variable in the config."
   (setq my-packages (remove (package-desc-name pkg) my-packages))
-  (my/write-packages-to-config))
+  (pc/write-packages-to-config))
 
 (ad-activate 'package-install)
 (ad-activate 'package-delete)
@@ -48,7 +48,7 @@
 (packages-install my-packages)
 
 ;; Seriously? Only UI delete for packages?
-(defun my/package-delete (package)
+(defun pc/package-delete (package)
   "Provide a way to delete packages from the CLI."
   (interactive
    (progn
