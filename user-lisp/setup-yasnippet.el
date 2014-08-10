f39c0ca9e3ad0018800cb6e323780dc827498579
Puneeth Chaganti <punchagan@muse-amuse.in>
2014-08-09 22:26:52 -0400

    Setup yasnippet

--- user-lisp/setup-yasnippet.el
+++ user-lisp/setup-yasnippet.el
@@ -1,5 +1,16 @@
 (require 'yasnippet)
 
-;; (yas/load-directory (expand-file-name "snippets" root-dir))
+(yas/load-directory
+ (expand-file-name
+  "snippets"
+  (file-name-directory (symbol-file 'yas-global-mode))))
+
+(let ((user-snippets-dir
+       (expand-file-name "snippets" user-emacs-directory)))
+  (when (file-exists-p user-snippets-dir)
+    (yas/load-directory user-snippets-dir)))
+
+(yas-global-mode 1)
+
 
 (provide 'setup-yasnippet)
