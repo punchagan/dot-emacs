98f19ebb49800e386c12e696164060facf94487d
Puneeth Chaganti <punchagan@muse-amuse.in>
2014-08-16 11:59:52 -0400

    Setup virtualenvs first when dealing with Py stuff.

--- user-lisp/setup-python.el
+++ user-lisp/setup-python.el
@@ -1,3 +1,17 @@
+;; Virtual environments
+(setq python-environment-directory "~/.virtualenvs/")
+
+(require 'virtualenvwrapper)
+(venv-initialize-interactive-shells) ;; if you want interactive shell support
+(venv-initialize-eshell) ;; if you want eshell support
+(setq venv-location "~/.virtualenvs")
+
+;; Pyflakes
+(require 'flycheck-pyflakes)
+(add-hook 'python-mode-hook 'flycheck-mode)
+(add-to-list 'flycheck-disabled-checkers 'python-flake8)
+(add-to-list 'flycheck-disabled-checkers 'python-pylint)
+
 ;; Cython mode
 (add-to-list 'auto-mode-alist '("\\.pyx\\'" . cython-mode))
 (add-to-list 'auto-mode-alist '("\\.pxd\\'" . cython-mode))
@@ -7,19 +21,6 @@
 (require 'jedi)
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:complete-on-dot t)
-
-;; Pyflakes
-(require 'flycheck-pyflakes)
-(add-hook 'python-mode-hook 'flycheck-mode)
-(add-to-list 'flycheck-disabled-checkers 'python-flake8)
-(add-to-list 'flycheck-disabled-checkers 'python-pylint)
-
-;; Virtual environments
-(setq python-environment-directory "~/.virtualenvs/")
-
-(require 'virtualenvwrapper)
-(venv-initialize-interactive-shells) ;; if you want interactive shell support
-(venv-initialize-eshell) ;; if you want eshell support
-(setq venv-location "~/.virtualenvs")
+(jedi:install-server)
 
 (provide 'setup-python)
