bfe4b17bbc6753846551f34e672ba8c8ca7d2474
Puneeth Chaganti <punchagan@muse-amuse.in>
2014-08-16 12:01:25 -0400

    Add marmalade to list of packages.

--- user-lisp/setup-package-list.el
+++ user-lisp/setup-package-list.el
@@ -1,5 +1,8 @@
 ;; CAUTION: Do not edit by hand, unless you know what you are doing!
-(defvar my-packages '(julia-mode logito metaweblog org
+(defvar my-packages '(auto-complete color-theme concurrent ctable
+  dash deferred direx el-get epc epl flycheck flycheck-pyflakes
+  gh gist git-commit-mode git-rebase-mode helm helm-themes jedi
+  julia-mode logito magit markdown-mode metaweblog org
   org-tree-slide org2blog ox-reveal paredit paredit-everywhere
   pcache pkg-info popup python-environment s tabulated-list
   virtualenvwrapper xml-rpc yasnippet))
--- user-lisp/setup-package.el
+++ user-lisp/setup-package.el
@@ -4,6 +4,7 @@
 (setq package-archives
       '(("gnu" . "http://elpa.gnu.org/packages/")
         ("melpa" . "http://melpa.milkbox.net/packages/")
+        ("marmalade" . "http://marmalade-repo.org/packages/")
         ("org" . "http://orgmode.org/elpa/")))
 
 (package-initialize)
