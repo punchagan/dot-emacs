c5b8778217c3014c73f8066e1028eab3021e1f51
Puneeth Chaganti <punchagan@muse-amuse.in>
2014-09-08 13:12:44 -0400

    Use org-plus-contrib, instead of org.

--- user-lisp/setup-package-list.el
+++ user-lisp/setup-package-list.el
@@ -1,9 +1,10 @@
 ;; CAUTION: Do not edit by hand, unless you know what you are doing!
 (defvar my-packages '(auto-complete coffee-mode color-theme
-  concurrent ctable dash deferred direx el-get epc epl flycheck
-  flycheck-pyflakes gh gist git-commit-mode git-rebase-mode helm
-  helm-themes jedi jsx-mode julia-mode logito magit markdown-mode
-  metaweblog org org-tree-slide org2blog ox-reveal paredit
+  concurrent connection ctable dash deferred dictionary direx
+  el-get epc epl flycheck flycheck-pyflakes gh gist
+  git-commit-mode git-rebase-mode helm helm-themes jedi jsx-mode
+  julia-mode link logito magit markdown-mode metaweblog
+  org-plus-contrib org-tree-slide org2blog ox-reveal paredit
   paredit-everywhere pcache pkg-info popup python-environment s
   tabulated-list virtualenvwrapper xml-rpc yasnippet))
 
