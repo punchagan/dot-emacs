03ade2212cf47cbbbaf3a9d52bff04304846feef
Puneeth Chaganti <punchagan@muse-amuse.in>
2014-11-04 22:34:44 +0530

    Use auth-source instead of netrc.

--- user-lisp/setup-blogging.el
+++ user-lisp/setup-blogging.el
@@ -2,22 +2,28 @@
 
 ;; org2blog
 (require 'metaweblog)
+(require 'auth-source)
 (require 'org2blog-autoloads)
 
-(setq org2blog/wp-blog-alist
-      '(("lafootrix"
-	 :url "http://lafootrix.wordpress.com/xmlrpc.php"
-	 :username "punchagan"
-	 :default-title "Hello World"
-	 :default-categories ("org2blog" "emacs")
-	 :tags-as-categories nil)
-	("org2blog"
-	 :url "http://testorg2blog.wordpress.com/xmlrpc.php"
-	 :username "org2blog"
-	 :default-title "Hello World"
-	 :default-categories ("org2blog" "emacs")
-	 :tags-as-categories nil
-	 :wp-code t)))
+(setq blog nil)
+
+
+(let ((credentials (auth-source-user-and-password "org2blog")))
+  (setq org2blog/wp-blog-alist
+        `(("lafootrix"
+           :url "http://lafootrix.wordpress.com/xmlrpc.php"
+           :username "punchagan"
+           :default-title "Hello World"
+           :default-categories ("org2blog" "emacs")
+           :tags-as-categories nil)
+          ("org2blog"
+           :url "http://testorg2blog.wordpress.com/xmlrpc.php"
+           :username ,(car credentials)
+           :password ,(cadr credentials)
+           :default-title "Hello World"
+           :default-categories ("org2blog" "emacs")
+           :tags-as-categories nil
+           :wp-code t))))
 
 (setq org2blog/wp-use-sourcecode-shortcode t)
 
