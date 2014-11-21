e7cba97ce3be8fbb4fae4715f7e1aea5022c8c44
Puneeth Chaganti <punchagan@muse-amuse.in>
2014-11-21 17:32:58 +0530

    Update current list of installed packages.

--- user-lisp/setup-package-list.el
+++ user-lisp/setup-package-list.el
@@ -1,11 +1,14 @@
 ;; CAUTION: Do not edit by hand, unless you know what you are doing!
-(defvar my-packages '(auto-complete coffee-mode color-theme
-  concurrent connection ctable dash deferred dictionary direx
-  el-get epc epl flycheck flycheck-pyflakes gh gist
-  git-commit-mode git-rebase-mode helm helm-themes jedi jsx-mode
-  julia-mode link logito magit markdown-mode metaweblog
-  org-plus-contrib org-tree-slide org2blog ox-reveal paredit
-  paredit-everywhere pcache pkg-info popup python-environment s
-  tabulated-list virtualenvwrapper xml-rpc yasnippet))
+(defvar my-packages '(auto-complete bind-key clip2org coffee-mode
+  color-theme concurrent connection ctable dash db debbugs
+  deferred dictionary diminish direx el-get elnode epc epl fakir
+  flycheck flycheck-pyflakes gh gist git-commit-mode
+  git-rebase-mode helm helm-themes jabber jedi jsx-mode
+  julia-mode kv lcs link logito lui magit markdown-mode
+  metaweblog noflet org-mode org-plus-contrib org-tree-slide
+  org2blog ox-reveal paredit paredit-everywhere pcache pkg-info
+  popup python-environment s sensitive sequences shorten
+  tabulated-list tracking use-package virtualenvwrapper web
+  xml-rpc xml-rpc-el yasnippet))
 
 (provide 'setup-package-list)
