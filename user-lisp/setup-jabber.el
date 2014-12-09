962245ce74456f56ea58b457618dd483ff1885a7
Puneeth Chaganti <punchagan@muse-amuse.in>
2014-12-10 00:05:29 +0530

    Create a json based jabber message queue like smtpmail.
    When offline, jabber messages get queued into a json file.  The queue is
    inspected and flushed when I connect back to jabber.

--- user-lisp/setup-jabber.el
+++ user-lisp/setup-jabber.el
@@ -31,4 +31,64 @@
 ;; make URLs clickable
 (add-hook 'jabber-chat-mode-hook 'goto-address)
 
+
+;;;; Jabber Message Queue stuff ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'json)
+
+(defconst pc/jabber-message-queue-file
+  (expand-file-name ".jabber-message-queue.json" user-emacs-directory)
+  "The file where jabber messages are queued")
+
+(defun pc/jabber-add-message-to-queue (to body)
+  "Queue up messages when offline, and send on connect."
+  (interactive (list
+                     (read-string "message: ")))
+  (let ((data (append
+               (or (ignore-errors (json-read-file pc/jabber-message-queue-file)) '())
+               `(((to . ,to) (body . ,body))))))
+
+    (with-temp-buffer
+      (insert (json-encode data))
+      (write-file pc/jabber-message-queue-file nil))))
+
+(defun pc/jabber-chat-buffer-send ()
+  "Send the message in the chat buffer to the queue."
+  (interactive)
+  (let ((body (delete-and-extract-region jabber-point-insert (point-max))))
+    (funcall 'pc/jabber-add-message-to-queue jabber-chatting-with body)))
+
+(defun pc/jabber-chat-with ()
+  "Queue messages if not connected, else normal chat."
+  (interactive)
+  (if (ignore-errors (jabber-read-account))
+      (call-interactively 'jabber-chat-with)
+    (let ((to (completing-read
+               "chat with: "
+               ;; yes, that's right! we use mu4e contact list!
+               (mapcar (lambda (x) (plist-get x :mail)) mu4e~contact-list))))
+      (switch-to-buffer (jabber-chat-create-buffer nil to))
+      (local-set-key (kbd "RET") 'pc/jabber-chat-buffer-send))))
+
+(defun pc/jabber-flush-queue (jc)
+  "Send all queued messages and empty queue."
+  (let ((data (or (ignore-errors (json-read-file pc/jabber-message-queue-file)) '())))
+    ;; Send messages
+    (mapcar
+     (lambda (x) (let ((to (cdr (assoc 'to x)))
+                       (body (cdr (assoc 'body x))))
+                   (message (format "Sent message to %s: %s" to body))
+                   (jabber-send-message jc to nil body "chat")))
+     data)
+
+    ;; Delete queue file
+    (delete-file pc/jabber-message-queue-file)
+
+    ;; Restore keymap
+    (define-key jabber-chat-mode-map (kbd "RET") 'jabber-chat-buffer-send)))
+
+(add-hook 'jabber-post-connect-hooks 'pc/jabber-flush-queue)
+
+;; Bind key to our chat function
+(global-set-key (kbd "C-x C-j C-j") 'pc/jabber-chat-with)
+
 (provide 'setup-jabber)
