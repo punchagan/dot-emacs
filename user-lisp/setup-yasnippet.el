2f42d47fce2f1b3a97009a2a78c2c36117edf86d
Puneeth Chaganti <punchagan@muse-amuse.in>
2015-01-02 13:46:37 +0530

    All yas/snippets need not be loaded at startup.
    Shaves off 0.5seconds from startup.

--- user-lisp/setup-yasnippet.el
+++ user-lisp/setup-yasnippet.el
@@ -1,16 +1,3 @@
 (require 'yasnippet)
-
-(yas/load-directory
- (expand-file-name
-  "snippets"
-  (file-name-directory (symbol-file 'yas-global-mode))))
-
-(let ((user-snippets-dir
-       (expand-file-name "snippets" user-emacs-directory)))
-  (when (file-exists-p user-snippets-dir)
-    (yas/load-directory user-snippets-dir)))
-
 (yas-global-mode 1)
-
-
 (provide 'setup-yasnippet)
