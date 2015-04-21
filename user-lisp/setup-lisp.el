ebff0d3921b0dd7cfc85820f3ac057b23cc85271
Puneeth Chaganti <punchagan@muse-amuse.in>
2015-04-22 04:44:06 +0530

    Move all stuff from user-lisp/ to punchagan.org

--- user-lisp/setup-lisp.el
+++
@@ -1,9 +0,0 @@
-;;; Lisp mode configuration
-
-(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
-(add-hook 'emacs-lisp-mode-hook 'pc/remove-elc-on-save)
-(add-hook 'emacs-lisp-mode-hook 'pc/turn-on-paredit)
-
-(define-key emacs-lisp-mode-map (kbd "C-c v") 'eval-buffer)
-
-(provide 'setup-emacs-lisp)
--- user-lisp/setup-yasnippet.el
+++
@@ -1,3 +0,0 @@
-(require 'yasnippet)
-(yas-global-mode 1)
-(provide 'setup-yasnippet)
