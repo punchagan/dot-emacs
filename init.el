330a131a1af8ee9652ad6d6064ed067e05a85448
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-11-17 19:49:09 +0530

    Add code to screenshot Emacs

--- init.el
+++ init.el
@@ -397,6 +397,19 @@
 (setq-default fill-column 79)
 ;; Fill column:1 ends here
 
+;; Screenshots:1 starts here
+(defun pc/screenshot-svg ()
+  "Save a screenshot of the current frame as an SVG image.
+Saves to a temp file and puts the filename in the kill ring."
+  (interactive)
+  (let* ((filename (make-temp-file "Emacs" nil ".svg"))
+         (data (x-export-frames nil 'svg)))
+    (with-temp-file filename
+      (insert data))
+    (kill-new filename)
+    (message filename)))
+;; Screenshots:1 ends here
+
 ;; magit and git related stuff:1 starts here
 (use-package magit
   :bind
